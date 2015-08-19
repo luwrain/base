6b3b3874a8b1f3849b34eb229692ac26eb4b906a
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2015-08-20 05:12:25 +0600

    Towards synchronous events
    - core.Event class gets markAsProcessed() and waitForBeProcessed() methods
      (the safe method to ensure that the event is processed but only from another thread)
    - core.Event class gets UI_EVENT and AREA_QUERY constants
    - Event.type() method renamed to Event.eventType()

--- src/main/java/org/luwrain/core/Event.java
+++ src/main/java/org/luwrain/core/Event.java
@@ -18,18 +18,59 @@ package org.luwrain.core;
 
 public class Event
 {
-    public static final int KEYBOARD_EVENT = 1;
+    static public final int KEYBOARD_EVENT = 1;
     public static final int ENVIRONMENT_EVENT = 2;
+    public static final int AREA_QUERY_EVENT = 3;
+    public static final int UI_EVENT = 4;
 
-    private int type;
+    private int eventType;
+    private boolean processed = false;
+    private Object syncObj = new Object();
 
-    public Event(int type)
+    public Event(int eventType)
     {
-	this.type = type;
+	this.eventType = eventType;
     }
 
-    int type()
+    public int eventType()
     {
-	return type;
+	return eventType;
+    }
+
+    /**
+     * Signals that the processing of this event is finished. Do not touch
+     * this method as it is designed for invocation by 
+     * {@code org.luwrain.core.Environment} class only which controls the event
+     * loop. Once this method is called, all threads freezed on {@code waitForBeProcessed()}
+     * method continue the execution.
+     */
+    public void markAsProcessed()
+    {
+	if (processed)
+	    return;
+	processed = true;
+	synchronized (syncObj) 
+	{
+	    syncObj.notifyAll();
+	}
+    }
+
+    /**
+     * Freezes current thread until this event be processed. This method
+     * guarantees that the execution will continue after the core completely
+     * finishes processing of the event. This method may not be used in the
+     * same thread as the main thread of the core (there are no any
+     * corresponding checks). The improper use of this method will hang the
+     * system infinitely.
+     */
+    public void waitForBeProcessed() throws InterruptedException
+    {
+	if (processed)
+	    return;
+	synchronized (syncObj)
+	{
+	    while (!processed)
+		syncObj.wait();
+	}
     }
 }
