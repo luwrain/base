71158024d84ed80bb0d4656643ab2536712d70f4
Michael Pozhidaev <msp@altlinux.ru>
2015-09-22 21:35:40 +0600

    lwr-prepare script
    - lwr-prepare script added
    - Various fixes of scripts for extensions support, doctree, etc

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,3 +1,4 @@
 
-APPS='notepad news fetch commander twitter reader message mail contacts'
-LANGS='en'
+COMPONENTS='luwrain linux pim i18n registry windows doctree extensions'
+APPS='notepad news fetch commander twitter reader message mail term'
+LANGS='ru'
