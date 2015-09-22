f04b5ae3b90f36a193cd1f372743b9efc5e24491
Michael Pozhidaev <msp@altlinux.ru>
2015-09-22 21:54:16 +0600

    lwr-dist-src script
    - scripts/lwr-dist-src added for quick creating the tree of sources

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,3 +2,5 @@
 COMPONENTS='luwrain linux pim i18n registry windows doctree extensions'
 APPS='notepad news fetch commander twitter reader message mail term'
 LANGS='ru'
+
+SRC_DEST_DIR=/tmp/luwrain-src
