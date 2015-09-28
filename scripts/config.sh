ab31cef4d6c4c46d2ebdf8edcbbc3d9123cd86bc
Michael Pozhidaev <msp@altlinux.ru>
2015-09-28 13:46:17 +0600

    Scripts update: hsqldb creating and extensions
    - Proper creating of hsqldb files added on lwr-prepare call
    - Extensions compilation added

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,6 +1,8 @@
 
 COMPONENTS='luwrain linux pim i18n registry windows doctree extensions'
-APPS='notepad news fetch commander twitter reader message mail term'
+APPS='notepad news fetch commander twitter reader message mail term contacts'
 LANGS='ru'
+EXTENSIONS='yatran'
+DBS='news contacts mail'
 
 SRC_DEST_DIR=/tmp/luwrain-src
