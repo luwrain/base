799e192472badc4b4d3e63cbdc347735052f309d
Michael Pozhidaev <msp@altlinux.ru>
2015-09-30 15:01:40 +0600

    Browser integration
    - app-browser and interaction-javafx included to compilation scripts
    - /bin/sh in the scripts replaced with /bin/bash
    - Creating of DESKTOP.ru.txt added to lwr-prepare

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,6 +1,6 @@
 
-COMPONENTS='luwrain linux pim i18n registry windows doctree extensions'
-APPS='notepad news fetch commander twitter reader message mail term contacts'
+
+APPS='notepad news fetch commander twitter reader message mail term contacts browser'
 LANGS='ru'
 EXTENSIONS='yatran'
 DBS='news contacts mail'
