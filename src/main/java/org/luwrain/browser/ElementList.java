04b8bb16467191e9dcb8175e64fd18df24e42c63
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2015-10-15 03:47:17 +0600

    No longer interface for splitted lines processing in browser.ElementList

--- src/main/java/org/luwrain/browser/ElementList.java
+++ src/main/java/org/luwrain/browser/ElementList.java
@@ -1,11 +1,11 @@
 
 package org.luwrain.browser;
 
-import org.luwrain.browser.ElementList.SplitedLine;
+//import org.luwrain.browser.ElementList.SplittedLine;
 
 public interface ElementList
 {
-    public static abstract interface Selector
+    public interface Selector
     {
 	boolean check(ElementList wel);
 	boolean first(ElementList wel);
@@ -14,22 +14,22 @@ public interface ElementList
 	boolean to(ElementList wel,int pos);
     }
 
-    public static interface SelectorALL extends Selector
+    public interface SelectorALL extends Selector
     {
 	boolean isVisible();
 	void setVisible(boolean visible);
 	boolean checkVisible(ElementList wel);
-	boolean check(ElementList wel);
+	//	boolean check(ElementList wel);
     }
 
-    public static interface SelectorTEXT extends Selector
+    public interface SelectorTEXT extends Selector
     {
 	String getFilter();
 	void setFilter(String filter);
-	boolean check(ElementList wel);
+	//	boolean check(ElementList wel);
     }
 
-    public static interface SelectorCSS extends Selector
+    public interface SelectorCSS extends Selector
     {
 	String getTagName();
 	void setTagName(String tagName);
@@ -38,7 +38,7 @@ public interface ElementList
 	String getStyleValue();
 	void setStyleValue(String styleValue);
 
-	boolean check(ElementList wel);
+	//	boolean check(ElementList wel);
     }
 
     public static interface SelectorTAG extends SelectorALL
@@ -65,25 +65,4 @@ public interface ElementList
     String getComputedStyleAll();
 
     void clickEmulate();
-	
-	static public class SplitedLine
-	{
-		public String type;
-		public String text;
-		public int pos; // element position in domidx
-		public int index; // line index in global line count
-		public SplitedLine(String type,String text,int pos,int index)
-		{
-			this.type=type;
-			this.text=text;
-			this.pos=pos;
-			this.index=index;
-		}
-	};
-
-	SplitedLine[][] getSplitedLines();
-	int getSplitedCount();
-	void splitAllElementsTextToLines(int width,Selector selector);
-	SplitedLine getSplitedLineByIndex(int index);
-	SplitedLine[] getSplitedLineByPos(int pos);
 }
