6e432f9c3b269c55624b58bb39378a929ef27872
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-02-14 18:06:41 +0600

    New approach to keyboard codes
    - Special codes stored as an enum
    - KeyboardEvent.isCommand() renamed to isSpecial()
    - KeyboardEvent.getCommand() renamed to getSpecial() and now returns enum
    - KeyboardEvent class gets static method translateSpecial()

--- src/main/java/org/luwrain/core/events/KeyboardEvent.java
+++ src/main/java/org/luwrain/core/events/KeyboardEvent.java
@@ -23,70 +23,29 @@ import org.luwrain.core.*;
 
 public class KeyboardEvent extends Event
 {
-    static public final int ENTER = 0;
-    static public final int BACKSPACE = 1;
-    static public final int ESCAPE = 2;
-    static public final int TAB = 3;
-
-    static public final int ARROW_DOWN = 10;
-    static public final int ARROW_UP = 11;
-    static public final int ARROW_LEFT = 12;
-    static public final int ARROW_RIGHT = 13;
-
-    static public final int INSERT = 20;
-    static public final int DELETE = 21;
-    static public final int HOME = 22;
-    static public final int END = 23;
-    static public final int PAGE_UP = 24;
-    static public final int PAGE_DOWN = 25;
-
-    static public final int F1 = 30;
-    static public final int F2 = 31;
-    static public final int F3 = 32;
-    static public final int F4 = 33;
-    static public final int F5 = 34;
-    static public final int F6 = 35;
-    static public final int F7 = 36;
-    static public final int F8 = 37;
-    static public final int F9 = 38;
-    static public final int F10 = 39;
-    static public final int F11 = 40;
-    static public final int F12 = 41;
-
-    static public final int WINDOWS = 50;
-    static public final int CONTEXT_MENU = 51;
-
-    static public final int SHIFT = 60;
-    static public final int CONTROL = 61;
-    static public final int LEFT_ALT = 62;
-    static public final int RIGHT_ALT = 63;
-
-    static public final int ALTERNATIVE_ARROW_DOWN = 70;
-    static public final int ALTERNATIVE_ARROW_UP = 71;
-    static public final int ALTERNATIVE_ARROW_LEFT = 72;
-    static public final int ALTERNATIVE_ARROW_RIGHT = 73;
-    static public final int ALTERNATIVE_HOME = 74;
-    static public final int ALTERNATIVE_END = 75;
-    static public final int ALTERNATIVE_PAGE_UP = 76;
-    static public final int ALTERNATIVE_PAGE_DOWN = 77;
-    static public final int ALTERNATIVE_DELETE = 78;
+    public enum Special {
+	ENTER, BACKSPACE, ESCAPE, TAB,
+	ARROW_DOWN, ARROW_UP, ARROW_LEFT, ARROW_RIGHT,
+	INSERT, DELETE, HOME, END, PAGE_UP, PAGE_DOWN,
+	F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12,
+	WINDOWS, CONTEXT_MENU,
+	SHIFT, CONTROL, LEFT_ALT, RIGHT_ALT,
+	ALTERNATIVE_ARROW_DOWN, ALTERNATIVE_ARROW_UP, ALTERNATIVE_ARROW_LEFT, ALTERNATIVE_ARROW_RIGHT,
+	ALTERNATIVE_HOME, ALTERNATIVE_END, ALTERNATIVE_PAGE_UP, ALTERNATIVE_PAGE_DOWN, ALTERNATIVE_DELETE,
+    };
 
     private boolean cmd = false;
-    private int cmdCode = 0;
+    private Special cmdCode = null;
     private char nonCmdChar = 0;
-
     private boolean shiftPressed = false;
     private boolean controlPressed = false;
     private boolean leftAltPressed = false;
     private boolean rightAltPressed = false;
 
     public KeyboardEvent(boolean cmd,
-			 int cmdCode,
-			 char nonCmdChar,
-			 boolean shiftPressed,
-			 boolean controlPressed,
-			 boolean leftAltPressed,
-			 boolean rightAltPressed)
+			 Special cmdCode, char nonCmdChar,
+			 boolean shiftPressed, boolean controlPressed,
+			 boolean leftAltPressed, boolean rightAltPressed)
     {
 	super(KEYBOARD_EVENT);
 	this.cmd = cmd;
@@ -99,8 +58,7 @@ public class KeyboardEvent extends Event
     }
 
     public KeyboardEvent(boolean cmd,
-			 int cmdCode,
-			 char nonCmdChar)
+			 Special cmdCode, char nonCmdChar)
     {
 	super(KEYBOARD_EVENT);
 	this.cmd = cmd;
@@ -111,6 +69,7 @@ public class KeyboardEvent extends Event
 	leftAltPressed = false;
 	rightAltPressed = false;
     }
+
     /*FIXME:It is better to rename it to equalKeysOnKeyboard()*/
     public boolean equals(KeyboardEvent event)
     {
@@ -123,17 +82,17 @@ public class KeyboardEvent extends Event
 		rightAltPressed == event.rightAltPressed);
     }
 
-    public boolean isCommand()
+    public boolean isSpecial()
     {
 	return cmd;
     }
 
-    public char getCharacter()
+    public char getChar()
     {
 	return nonCmdChar;
     }
 
-    public int getCommand()
+    public Special getSpecial()
     {
 	return cmdCode;
     }
@@ -193,7 +152,7 @@ public class KeyboardEvent extends Event
 	return rightAltPressed && !leftAltPressed && !shiftPressed && !controlPressed;
     }
 
-    public String toString()
+    @Override public String toString()
     {
 	String res = cmd?("[CMD] " + cmdCode):("\'" + nonCmdChar + "\'");
 	if (shiftPressed)
@@ -205,5 +164,83 @@ public class KeyboardEvent extends Event
 	if (rightAltPressed)
 	    res += " RIGHT-ALT";
 	return res;
+    }
+
+    static public Special translateSpecial(String value)
+    {
+	NullCheck.notNull(value, "value");
+	if (value.trim().isEmpty())
+	    throw new IllegalArgumentException("value may not be empty");
+	switch(value.trim().toLowerCase())
+	{
+	case "enter":
+	    return Special.ENTER;
+	case "backspace":
+	    return Special.BACKSPACE;
+	case "escape":
+	    return Special.ESCAPE;
+	case "tab":
+	    return Special.TAB;
+	case "ARROW_DOWN":
+	    return Special.ARROW_DOWN;
+	case "arrow-up":
+	    return Special.ARROW_UP;
+	case "arrow-left":
+	    return Special.ARROW_LEFT;
+	case "arrow-right":
+	    return Special.ARROW_RIGHT;
+	case "insert":
+	    return Special.INSERT;
+	case "delete":
+	    return Special.DELETE;
+	case "alternative-delete":
+	    return Special.ALTERNATIVE_DELETE;
+	case "home":
+	    return Special.HOME;
+	case "end":
+	    return Special.END;
+	case "page-up":
+	    return Special.PAGE_UP;
+	case "page-down":
+	    return Special.PAGE_DOWN;
+	case "f1":
+	    return Special.F1;
+	case "f2":
+	    return Special.F2;
+	case "f3":
+	    return Special.F3;
+	case "f4":
+	    return Special.F4;
+	case "f5":
+	    return Special.F5;
+	case "f6":
+	    return Special.F6;
+	case "f7":
+	    return Special.F7;
+	case "f8":
+	    return Special.F8;
+	case "f9":
+	    return Special.F9;
+	case "f10":
+	    return Special.F10;
+	case "f11":
+	    return Special.F11;
+	case "f12":
+	    return Special.F12;
+	case "windows":
+	    return Special.WINDOWS;
+	case "context-menu":
+	    return Special.CONTEXT_MENU;
+	case "shift":
+	    return Special.SHIFT;
+	case "control":
+	    return Special.CONTROL;
+	case "left-alt":
+	    return Special.LEFT_ALT;
+	case "right-alt":
+	    return Special.RIGHT_ALT;
+	default:
+	    return null;
+	}
     }
 }
