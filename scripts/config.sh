538461f98e5c1956475c7fdf795a78465024dad4
Michael Pozhidaev <msp@altlinux.ru>
2016-12-25 06:43:40 +0700

    Adding lwr-distr-linux-pkg script

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,19 +1,28 @@
 
 
 GITS='base doctree education extensions i18n interaction-javafx linux luwrain network pim registry windows'
+DBS='news contacts mail'
+LANGS='en ru'
+APPS='notepad news fetch commander twitter reader message mail term contacts browser player chat writer'
+EXTENSIONS='voiceman emacspeak cmdtts yatran mssapi rhvoice'
+
 BASIC_COMPONENTS='base pim doctree interaction-javafx education'
 LINUX_COMPONENTS='linux network'
 WINDOWS_COMPONENTS='windows'
 COMPONENTS="$BASIC_COMPONENTS $LINUX_COMPONENTS $WINDOWS_COMPONENTS"
-APPS='notepad news fetch commander twitter reader message mail term contacts browser player chat'
-LANGS='en ru'
+
+LINUX_ARCH=64
+LINUX_LANGS='ru'
+LINUX_APPS='notepad news fetch commander twitter reader message mail term contacts browser player chat'
+LINUX_EXTENSIONS='voiceman emacspeak cmdtts yatran rhvoice'
+
 ISO_LANGS='en ru'
+
 WIN_LANGS='ru'
-EXTENSIONS='voiceman emacspeak cmdtts yatran mssapi rhvoice'
-DBS='news contacts mail'
-TAKE_SCRIPTS='network linux app-reader'
-TAKE_PROPERTIES='education luwrain'
 WINDOWS_DIST_EXTENSIONS='yatran mssapi rhvoice'
+
+TAKE_SCRIPTS='network linux app-reader'
+TAKE_PROPERTIES='luwrain'
 SDK_JARS='base pim doctree network linux windows'
 
 SRC_DEST_DIR=/tmp/luwrain-src
