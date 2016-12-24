0fe7fbacde8728448359c212c8fc4f26551c33d4
Michael Pozhidaev <msp@altlinux.ru>
2016-12-25 06:54:44 +0700

    Old lwr-distr-linux script becomes lwr-distr-linux-nojre

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,18 +1,21 @@
 
 
 GITS='base doctree education extensions i18n interaction-javafx linux luwrain network pim registry windows'
+PROPERTIES_FILE=release.properties
+
+# Used for compilation, everything must be listed regardless the target platform
 DBS='news contacts mail'
 LANGS='en ru'
 APPS='notepad news fetch commander twitter reader message mail term contacts browser player chat writer'
 EXTENSIONS='voiceman emacspeak cmdtts yatran mssapi rhvoice'
+COMPONENTS='base pim doctree interaction-javafx education linux windows network'
 
 BASIC_COMPONENTS='base pim doctree interaction-javafx education'
-LINUX_COMPONENTS='linux network'
 WINDOWS_COMPONENTS='windows'
-COMPONENTS="$BASIC_COMPONENTS $LINUX_COMPONENTS $WINDOWS_COMPONENTS"
 
 LINUX_ARCH=64
 LINUX_LANGS='ru'
+LINUX_COMPONENTS='base pim doctree interaction-javafx education linux'
 LINUX_APPS='notepad news fetch commander twitter reader message mail term contacts browser player chat'
 LINUX_EXTENSIONS='voiceman emacspeak cmdtts yatran rhvoice'
 
@@ -24,6 +27,4 @@ WINDOWS_DIST_EXTENSIONS='yatran mssapi rhvoice'
 TAKE_SCRIPTS='network linux app-reader'
 TAKE_PROPERTIES='luwrain'
 SDK_JARS='base pim doctree network linux windows'
-
 SRC_DEST_DIR=/tmp/luwrain-src
-PROPERTIES_FILE=release.properties
