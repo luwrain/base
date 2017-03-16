7d6f7ef96244002414b9a39baafa3645a54093aa
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2017-03-16 14:15:41 +0700

    Adding of Urls.toUrl()

--- src/main/java/org/luwrain/util/Urls.java
+++ src/main/java/org/luwrain/util/Urls.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2012-2017 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
 
 package org.luwrain.util;
 
@@ -20,8 +35,17 @@ catch(URISyntaxException e)
 {
     return new File(url.getPath());
 }
+    }
 
-
-
+    static public URL toUrl(File file)
+    {
+	NullCheck.notNull(file, "file");
+	try {
+	    return file.toURI().toURL();
+	}
+	catch(IOException e)
+	{
+	    return null;
+	}
     }
 }
