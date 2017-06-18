ba2b25f0dbbe339fd01ede40c0274576211a483e
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2017-06-18 20:43:00 +0700

    Renaming ElementIterator to BrowserIterator

---
+++ src/main/java/org/luwrain/browser/BrowserIterator.java
@@ -0,0 +1,67 @@
+/*
+   Copyright 2012-2017 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+   Copyright 2015-2016 Roman Volovodov <gr.rPman@gmail.com>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+package org.luwrain.browser;
+
+import java.awt.Rectangle;
+
+public interface BrowserIterator
+{
+	/** return browser object for this ElementIterator */
+	Browser getBrowser();
+
+	/** element position in Browser's nodes list */
+    int getPos();
+
+	void setPos(int integer);
+
+    /** return text abstract type FIXME: remove it and move this functionality to app-browser */
+    String getType();
+
+    /** get computed node text */
+    String getText();
+
+    /** get list of values for HTML SELECT FIXME: it's awful */
+    String[] getMultipleText();
+
+    String getComputedText();
+
+    String getLink();
+
+    Rectangle getRect();
+
+    boolean isEditable();
+
+    void setText(String text);
+
+    String getAttributeProperty(String name);
+
+    String getComputedStyleProperty(final String name);
+
+    String getComputedStyleAll();
+
+    String getHtmlTagName();
+
+    void clickEmulate();
+
+BrowserIterator getParent();
+    boolean isVisible();
+    boolean forTEXT();
+BrowserIterator clone();
+	void submitEmulate();
+	String getAltText();
+}
