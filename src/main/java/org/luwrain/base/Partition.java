b3e3ca27dc1b8ab570ad9163fa167bff958b2304
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2017-07-10 06:23:04 +0700

    base.Partition is now an interface

--- src/main/java/org/luwrain/base/Partition.java
+++ src/main/java/org/luwrain/base/Partition.java
@@ -19,49 +19,18 @@ package org.luwrain.base;
 
 import java.io.File;
 
-public class Partition
+public interface Partition
 {
-    static public final int REGULAR = 0;
-    static public final int REMOVABLE = 1;
-    static public final int REMOTE = 2;
-    static public final int USER_HOME = 3;
-    static public final int ROOT = 4;
+    public enum Type {
+	REGULAR,
+	REMOVABLE,
+	REMOTE,
+	USER_HOME,
+	ROOT,
+    };
 
-    private int type;
-    private File file;
-    private String name;
-    private boolean mounted = false;
-
-    public Partition(int type, File file,
-	      String name, boolean mounted)
-    {
-	this.type = type;
-	this.file = file;
-	this.name = name;
-	this.mounted = mounted;
-	if (file == null)
-	    throw new NullPointerException("file may not be null");
-	if (name == null)
-	    throw new NullPointerException("name may not be null");
-    }
-
-    public int type()
-    {
-	return type;
-    }
-
-    public File file()
-    {
-	return file;
-    }
-
-public String name()
-{
-return name;
-}
-
-    public boolean mounted()
-    {
-	return mounted;
-    }
+    Type getPartType();
+    public File getPartFile();
+    public String getPartName();
+    public boolean isMounted();
 }
