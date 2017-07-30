b8d9ad9cc90494a551c815abbe789ad0a7aae915
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2017-07-31 04:53:56 +0700

    Removing app-chat, app-term and doctree, adding app-telegram and browser

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,35 +1,34 @@
 
 VERSION=1.1.0
-GITS='base doctree education extensions i18n interaction-javafx linux luwrain network pim registry windows'
+GITS='base browser extensions i18n interaction-javafx linux luwrain network pim registry windows'
 PROPERTIES_FILE=release.properties
 
 # Used for compilation, everything must be listed regardless the target platform
 DBS='news contacts mail'
 LANGS='en ru'
-APPS='notepad news fetch commander twitter reader message mail term contacts browser player chat writer'
+APPS='notepad news fetch commander twitter reader message mail contacts browser player telegram writer'
 EXTENSIONS='voiceman emacspeak cmdtts yatran mssapi rhvoice'
-COMPONENTS='base pim doctree interaction-javafx education linux windows network'
+COMPONENTS='base pim browser interaction-javafx linux windows network'
 
 WIN_ARCH=32
 WIN_LANGS='en ru'
-WIN_COMPONENTS='base pim doctree interaction-javafx education windows'
-WIN_APPS='notepad news fetch commander twitter reader message mail contacts browser player chat'
+WIN_COMPONENTS='base pim browser interaction-javafx windows'
+WIN_APPS='notepad news fetch commander twitter reader message mail contacts browser player telegram'
 WIN_EXTENSIONS='mssapi cmdtts yatran rhvoice'
 
 LINUX_ARCH='32 64'
 LINUX_LANGS='en ru'
-LINUX_COMPONENTS='base pim doctree interaction-javafx education linux'
-LINUX_APPS='notepad news fetch commander twitter reader message mail term contacts browser player chat'
+LINUX_COMPONENTS='base pim browser interaction-javafx linux'
+LINUX_APPS='notepad news fetch commander twitter reader message mail contacts browser player telegram'
 LINUX_EXTENSIONS='voiceman emacspeak cmdtts yatran rhvoice'
 
 ISO_ARCH='32 64'
 ISO_LANGS='en ru'
-ISO_COMPONENTS='base pim doctree interaction-javafx education network linux'
-ISO_APPS='notepad news fetch commander twitter reader message mail term contacts browser player chat'
+ISO_COMPONENTS='base pim browser interaction-javafx network linux'
+ISO_APPS='notepad news fetch commander twitter reader message mail contacts browser player telegram'
 ISO_EXTENSIONS='voiceman emacspeak cmdtts yatran rhvoice'
-
 
 TAKE_SCRIPTS='network linux app-reader'
 TAKE_PROPERTIES='luwrain education'
-SDK_JARS='base pim doctree network linux windows'
+SDK_JARS='base pim browser network linux windows'
 SRC_DEST_DIR=/tmp/luwrain-src
