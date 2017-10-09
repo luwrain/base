513d00d05d0317027356ea21cdf1e01030921b5a
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2017-10-09 10:31:40 +0700

    Minor fixes to scripts

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,7 +2,7 @@
 VERSION=1.1.0
 GITS='base browser extensions i18n interaction-javafx linux luwrain network pim registry windows'
 PROPERTIES_FILE=release.properties
-JUNIT_COMPONENTS='browser'
+JUNIT_COMPONENTS='luwrain browser'
 
 # Used for compilation, everything must be listed regardless the target platform
 DBS='news contacts mail'
