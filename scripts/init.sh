736e7ad853b72110ceb3021457dc5a3ecf8bfc94
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-01-12 21:50:41 +0700

    Adding the lwr-ant-gen-app script

--- scripts/init.sh
+++ scripts/init.sh
@@ -10,6 +10,7 @@ fi
 
 PWD=$(pwd)
 SRC_DIR="${PWD%/base/scripts*}"
+SCRIPTS_DIR="$SRC_DIR/base/scripts"
 
 if ! [ -e "$SRC_DIR/base/scripts/init.sh" ]; then
     echo "$THIS:Something wrong with the current directory:no $SRC_DIR/base/scripts/init.sh"
