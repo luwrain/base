430db0fe7cdedeb5c784983b929607b51309932a
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-04-16 21:40:29 +0700

    Replacing network with io

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,36 +1,36 @@
 
 VERSION='(nightly)'
-GITS='base browser extensions i18n interaction-javafx linux luwrain network pim registry windows studio'
+GITS='base browser extensions i18n interaction-javafx luwrain io pim studio linux windows registry'
 PROPERTIES_FILE=release.properties
 JUNIT_COMPONENTS='base luwrain linux browser pim extensions/plmp3'
-SDK_JARS='base pim browser network studio'
+SDK_JARS='base pim browser io studio'
 SRC_DEST_DIR=/tmp/luwrain-src
 
 # Used for compilation, everything must be listed regardless the target platform
 LANGS='en ru'
 APPS='notepad news commander twitter reader mail contacts player telegram'
 EXTENSIONS='voiceman emacspeak cmdtts yatran mssapi rhvoice plmp3'
-COMPONENTS='base pim browser interaction-javafx linux windows network studio'
+COMPONENTS='base pim browser interaction-javafx linux windows io studio'
 
 WIN_ARCH=32
 WIN_LANGS='en ru'
-WIN_COMPONENTS='base pim browser interaction-javafx windows studio'
+WIN_COMPONENTS='base pim browser io interaction-javafx studio windows'
 WIN_APPS='notepad news commander twitter reader mail contacts player telegram'
 WIN_EXTENSIONS='mssapi cmdtts yatran rhvoice plmp3'
 WIN_PROPERTIES='luwrain windows app-player'
 
 LINUX_ARCH='32 64'
 LINUX_LANGS='en ru'
-LINUX_COMPONENTS='base pim browser interaction-javafx linux studio'
+LINUX_COMPONENTS='base pim browser interaction-javafx io studio linux'
 LINUX_APPS='notepad news commander twitter reader mail contacts player telegram'
 LINUX_EXTENSIONS='voiceman emacspeak cmdtts yatran rhvoice plmp3'
 LINUX_PROPERTIES='luwrain linux app-player'
-LINUX_SCRIPTS='network linux app-reader'
+LINUX_SCRIPTS='linux app-reader'
 
 ISO_ARCH='64'
 ISO_LANGS='en ru'
-ISO_COMPONENTS='base pim browser interaction-javafx network linux studio'
+ISO_COMPONENTS='base pim browser interaction-javafx io studio linux'
 ISO_APPS='notepad news commander twitter reader mail contacts player telegram'
 ISO_EXTENSIONS='voiceman emacspeak cmdtts yatran rhvoice plmp3'
 ISO_PROPERTIES='luwrain linux app-player'
-ISO_SCRIPTS='network linux app-reader'
+ISO_SCRIPTS='linux app-reader'
