66240282ce543d7152aab50fa3271fefeaebdb7c
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-06-23 06:44:21 +0700

    Excluding telegram from the list of apps

--- scripts/config.sh
+++ scripts/config.sh
@@ -8,21 +8,21 @@ SRC_DEST_DIR=/tmp/luwrain-src
 
 # Used for compilation, everything must be listed regardless the target platform
 LANGS='en ru'
-APPS='commander contacts mail news notepad player reader telegram twitter'
+APPS='commander contacts mail news notepad player reader twitter'
 EXTENSIONS='cmdtts emacspeak mssapi plmp3 rhvoice voiceman'
 COMPONENTS='base browser interaction-javafx io linux pim studio windows'
 
 WIN_ARCH=32
 WIN_LANGS='en ru'
 WIN_COMPONENTS='base browser interaction-javafx io pim studio windows'
-WIN_APPS='commander contacts mail news notepad player reader telegram twitter'
+WIN_APPS='commander contacts mail news notepad player reader twitter'
 WIN_EXTENSIONS='cmdtts mssapi plmp3 rhvoice'
 WIN_PROPERTIES='app-player luwrain windows'
 
 LINUX_ARCH='32 64'
 LINUX_LANGS='en ru'
 LINUX_COMPONENTS='base browser interaction-javafx io linux pim studio'
-LINUX_APPS='commander contacts mail news notepad player reader telegram twitter'
+LINUX_APPS='commander contacts mail news notepad player reader twitter'
 LINUX_EXTENSIONS='cmdtts emacspeak plmp3 rhvoice voiceman'
 LINUX_PROPERTIES='app-player linux luwrain'
 LINUX_SCRIPTS='app-reader linux'
@@ -30,7 +30,7 @@ LINUX_SCRIPTS='app-reader linux'
 ISO_ARCH='64'
 ISO_LANGS='en ru'
 ISO_COMPONENTS='base browser interaction-javafx io linux pim studio'
-ISO_APPS='commander contacts mail news notepad player reader telegram twitter'
+ISO_APPS='commander contacts mail news notepad player reader twitter'
 ISO_EXTENSIONS='cmdtts emacspeak plmp3 rhvoice voiceman'
 ISO_PROPERTIES='app-player linux luwrain'
 ISO_SCRIPTS='app-reader linux'
