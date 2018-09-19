82c1718706b1d5cac7ff892b4b699ab17e93e55a
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-09-19 11:59:32 +0700

    Adding vk as the new app

--- scripts/config.sh
+++ scripts/config.sh
@@ -8,21 +8,21 @@ SRC_DEST_DIR=/tmp/luwrain-src
 
 # Used for compilation, everything must be listed regardless the target platform
 LANGS='en ru'
-APPS='commander contacts mail news notepad player reader twitter'
+APPS='commander contacts mail news notepad player reader twitter vk'
 EXTENSIONS='cmdtts emacspeak mssapi plmp3 rhvoice voiceman'
 COMPONENTS='base browser interaction-javafx io linux packs pim studio windows'
 
 WIN_ARCH=32
 WIN_LANGS='en ru'
 WIN_COMPONENTS='base browser interaction-javafx io packs pim studio windows'
-WIN_APPS='commander contacts mail news notepad player reader twitter'
+WIN_APPS='commander contacts mail news notepad player reader twitter vk'
 WIN_EXTENSIONS='cmdtts mssapi plmp3 rhvoice'
 WIN_PROPERTIES='app-player app-reader luwrain windows'
 
 LINUX_ARCH='32 64'
 LINUX_LANGS='en ru'
 LINUX_COMPONENTS='base browser interaction-javafx io linux packs pim studio'
-LINUX_APPS='commander contacts mail news notepad player reader twitter'
+LINUX_APPS='commander contacts mail news notepad player reader twitter vk'
 LINUX_EXTENSIONS='cmdtts emacspeak plmp3 rhvoice voiceman'
 LINUX_PROPERTIES='app-player app-reader linux luwrain'
 LINUX_SCRIPTS='linux'
@@ -30,7 +30,7 @@ LINUX_SCRIPTS='linux'
 ISO_ARCH='64'
 ISO_LANGS='en ru'
 ISO_COMPONENTS='base browser interaction-javafx io linux packs pim studio'
-ISO_APPS='commander contacts mail news notepad player reader twitter'
+ISO_APPS='commander contacts mail news notepad player reader twitter vk'
 ISO_EXTENSIONS='cmdtts emacspeak plmp3 rhvoice voiceman'
 ISO_PROPERTIES='app-player app-reader linux luwrain'
 ISO_SCRIPTS='app-reader linux'
