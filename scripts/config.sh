e4878bfb8f3b400175e64282d99bc259ae7e8e8d
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-12-11 09:52:57 +0700

    Fixing using of player and reader in config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,6 +1,6 @@
 
 VERSION='1.1.2'
-GITS='base browser extensions i18n io interaction-javafx linux luwrain packs pim registry studio windows'
+GITS='base browser extensions i18n io interaction-javafx linux luwrain packs pim player reader registry studio windows'
 PROPERTIES_FILE=release.properties
 JUNIT_COMPONENTS='base luwrain linux browser io pim app-notepad'
 SDK_JARS='base browser io pim studio'
@@ -8,29 +8,29 @@ SRC_DEST_DIR=/tmp/luwrain-src
 
 # Used for compilation, everything must be listed regardless the target platform
 LANGS='en ro ru'
-APPS='commander contacts mail news notepad player reader twitter viewer vk'
+APPS='commander contacts mail news notepad twitter viewer vk'
 EXTENSIONS='cmdtts emacspeak mssapi plmp3 rhvoice voiceman'
-COMPONENTS='base browser interaction-javafx io linux packs pim studio windows'
+COMPONENTS='base browser interaction-javafx io linux packs pim player reader studio windows'
 
 WIN_ARCH=32
 WIN_LANGS='en ro ru'
-WIN_COMPONENTS='base browser interaction-javafx io packs pim studio windows'
-WIN_APPS='commander contacts mail news notepad player reader twitter viewer vk'
+WIN_COMPONENTS='base browser interaction-javafx io packs pim player reader studio windows'
+WIN_APPS='commander contacts mail news notepad twitter viewer vk'
 WIN_EXTENSIONS='cmdtts mssapi plmp3 rhvoice'
-WIN_PROPERTIES='app-player app-reader luwrain windows'
+WIN_PROPERTIES='luwrain player reader windows'
 
 LINUX_ARCH='32 64'
 LINUX_LANGS='en ro ru'
-LINUX_COMPONENTS='base browser interaction-javafx io linux packs pim studio'
-LINUX_APPS='commander contacts mail news notepad player reader twitter viewer vk'
+LINUX_COMPONENTS='base browser interaction-javafx io linux packs pim player reader studio'
+LINUX_APPS='commander contacts mail news notepad twitter viewer vk'
 LINUX_EXTENSIONS='cmdtts emacspeak plmp3 rhvoice voiceman'
-LINUX_PROPERTIES='app-player app-reader linux luwrain'
+LINUX_PROPERTIES='linux luwrain player reader'
 LINUX_SCRIPTS='linux'
 
 ISO_ARCH='64'
 ISO_LANGS='en ro ru'
-ISO_COMPONENTS='base browser interaction-javafx io linux packs pim studio'
-ISO_APPS='commander contacts mail news notepad player reader twitter viewer vk'
+ISO_COMPONENTS='base browser interaction-javafx io linux packs pim player reader studio'
+ISO_APPS='commander contacts mail news notepad twitter viewer vk'
 ISO_EXTENSIONS='cmdtts emacspeak plmp3 rhvoice voiceman'
-ISO_PROPERTIES='app-player app-reader linux luwrain'
-ISO_SCRIPTS='app-reader linux'
+ISO_PROPERTIES='linux luwrain player reader'
+ISO_SCRIPTS='linux'
