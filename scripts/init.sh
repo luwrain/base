a9361a65ed9a56d65a55e759bbdbef76f87fafea
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2019-02-13 08:48:09 +0700

    lwr-distr-win: Adding installer support to nightly builds

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,8 @@ THIS="${0##*/}"
 . config.sh
 
 PWD=$(pwd)
+# We may already be in the sources root, it's OK.
+# Scripts directory check will test this as well
 SRC_DIR="${PWD%/base/scripts*}"
 SCRIPTS_DIR="$SRC_DIR/base/scripts"
 
