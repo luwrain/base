971681865880f7e707b33bb319690b3170fd3cb2
Michael Pozhidaev <msp@luwrain.org>
2019-05-27 11:00:25 +0700

    Adding Init.createTempDataDir()

--- src/main/java/org/luwrain/core/Init.java
+++ src/main/java/org/luwrain/core/Init.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2012-2018 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+   Copyright 2012-2019 Michael Pozhidaev <msp@luwrain.org>
 
    This file is part of LUWRAIN.
 
@@ -25,9 +25,9 @@ import java.nio.charset.Charset;
 public final class Init
 {
     static public final String LOG_COMPONENT = "init";
-        static private final File DEBUG_FILE = new File(new File(System.getProperty("user.home")), "luwrain-debug.txt");
+    static private final File DEBUG_FILE = new File(new File(System.getProperty("user.home")), "luwrain-debug.txt");
 
-            static private final String ENV_APP_DATA = "APPDATA";
+    static private final String ENV_APP_DATA = "APPDATA";
     static private final String ENV_USER_PROFILE = "USERPROFILE";
     static private final String DEFAULT_USER_DATA_DIR_WINDOWS = "Luwrain";
     static private final String DEFAULT_USER_DATA_DIR_LINUX = ".luwrain";
@@ -39,32 +39,31 @@ public final class Init
 	    final PrintStream log = new PrintStream(new BufferedOutputStream(new FileOutputStream(DEBUG_FILE)), true);
 	    System.setOut(log);
 	    System.setErr(log);
-	}
-
-
+	} else
+	    Log.enableBriefMode();
 	final File userHomeDir = new File(System.getProperty("user.home"));
 	final File userDataDir = detectUserDataDir();
 	final File extDir = new File(userDataDir, "extensions");
-		    final List<URL> urls = new LinkedList();
-	    addJarsToClassPath(new File("jar"), urls);
-	    addJarsToClassPath(new File("lib"), urls);
-	    addExtensionsJarsToClassPath(extDir, urls);
-	    	    final ClassLoader classLoader = new URLClassLoader(urls.toArray(new java.net.URL[urls.size()]), ClassLoader.getSystemClassLoader());
-		    Thread.currentThread().setContextClassLoader(classLoader);
-		    		    setUtf8();
-	    final File dataDir = new File("data");
-	    final LaunchFactory factory;
-	    try {
-		final Object obj = Class.forName("org.luwrain.core.LaunchFactoryImpl", true, classLoader).newInstance();
+	final List<URL> urls = new LinkedList();
+	addJarsToClassPath(new File("jar"), urls);
+	addJarsToClassPath(new File("lib"), urls);
+	addExtensionsJarsToClassPath(extDir, urls);
+	final ClassLoader classLoader = new URLClassLoader(urls.toArray(new java.net.URL[urls.size()]), ClassLoader.getSystemClassLoader());
+	Thread.currentThread().setContextClassLoader(classLoader);
+	setUtf8();
+	final File dataDir = new File("data");
+	final LaunchFactory factory;
+	try {
+	    final Object obj = Class.forName("org.luwrain.core.LaunchFactoryImpl", true, classLoader).newInstance();
 	    factory = (LaunchFactory)obj;
-	    }
-	    catch(Throwable e)
-	    {
-		e.printStackTrace();
-		System.exit(1);
-		return;
-	    }
-	    factory.newLaunch(args, dataDir, userDataDir, userHomeDir).run();
+	}
+	catch(Throwable e)
+	{
+	    e.printStackTrace();
+	    System.exit(1);
+	    return;
+	}
+	factory.newLaunch(args, dataDir, userDataDir, userHomeDir).run();
     }
 
     static private void addExtensionsJarsToClassPath(File extensionsDir, List<URL> urls)
@@ -101,7 +100,6 @@ public final class Init
 	}
 	catch(IOException e)
 	{
-	    //	    Log.error(LOG_COMPONENT, "unable to add to the classpath the directory " + file.getAbsolutePath() + ":" + e.getClass().getName() + ":" + e.getMessage());
 	}
     }
 
@@ -138,7 +136,6 @@ public final class Init
 	return new File(f, DEFAULT_USER_DATA_DIR_LINUX);
     }
 
-    
     static private File createTempDataDir()
     {
 	try {
@@ -158,8 +155,4 @@ public final class Init
 	    return null;
 	}
     }
-
-
-
-    
 }
