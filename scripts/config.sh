83d634659a4bc351d5a60bbe6d210be600025d42
Michael Pozhidaev <msp@luwrain.org>
2019-05-31 12:11:50 +0700

    Enabling speechd extension

--- scripts/config.sh
+++ scripts/config.sh
@@ -10,7 +10,7 @@ SRC_DEST_DIR=/tmp/luwrain-src
 # Used for compilation, everything must be listed regardless the target platform
 LANGS='en fr ro ru'
 APPS='commander contacts mail news notepad twitter viewer vk'
-EXTENSIONS='cmdtts emacspeak mssapi plmp3 rhvoice voiceman'
+EXTENSIONS='cmdtts emacspeak mssapi plmp3 rhvoice speechd voiceman'
 COMPONENTS='base browser interaction-javafx io linux packs pim player reader studio windows'
 
 WIN_ARCH=32
@@ -24,7 +24,7 @@ LINUX_ARCH='32 64'
 LINUX_LANGS='en ro ru'
 LINUX_COMPONENTS='base browser interaction-javafx io linux packs pim player reader studio'
 LINUX_APPS='commander contacts mail news notepad twitter viewer vk'
-LINUX_EXTENSIONS='cmdtts emacspeak plmp3 rhvoice voiceman'
+LINUX_EXTENSIONS='cmdtts emacspeak plmp3 rhvoice speechd voiceman'
 LINUX_PROPERTIES='linux luwrain player reader'
 LINUX_SCRIPTS='linux'
 
@@ -32,6 +32,6 @@ ISO_ARCH='64'
 ISO_LANGS='en ro ru'
 ISO_COMPONENTS='base browser interaction-javafx io linux packs pim player reader studio'
 ISO_APPS='commander contacts mail news notepad twitter viewer vk'
-ISO_EXTENSIONS='cmdtts emacspeak plmp3 rhvoice voiceman'
+ISO_EXTENSIONS='cmdtts emacspeak plmp3 rhvoice speechd voiceman'
 ISO_PROPERTIES='linux luwrain player reader'
 ISO_SCRIPTS='linux'
