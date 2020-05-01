cae72df62261ce492752a50cbeee3d8bef980532
Michael Pozhidaev <msp@luwrain.org>
2020-05-01 15:22:11 +0700

    Including telegram to the apps

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,5 +1,5 @@
 
-VERSION=1.3.00
+VERSION=2.0.0
 REGISTRY_VERSION=1.3.00
 GITS='base browser extensions i18n io interaction-javafx linux luwrain packs pim player reader registry studio windows'
 PROPERTIES_FILE=release.properties
@@ -9,21 +9,21 @@ SRC_DEST_DIR=/tmp/luwrain-src
 
 # Used for compilation, everything must be listed regardless the target platform
 LANGS='en fr ro ru'
-APPS='commander contacts mail news notepad twitter viewer vk'
+APPS='commander contacts mail news notepad telegram twitter viewer vk'
 EXTENSIONS='cmdtts emacspeak mssapi plmp3 rhvoice speechd voiceman'
 COMPONENTS='base browser interaction-javafx io linux packs pim player reader studio windows'
 
 WIN_ARCH=32
 WIN_LANGS='en ro ru'
 WIN_COMPONENTS='base browser interaction-javafx io packs pim player reader studio windows'
-WIN_APPS='commander contacts mail news notepad twitter viewer vk'
+WIN_APPS='commander contacts mail news notepad telegram twitter viewer vk'
 WIN_EXTENSIONS='mssapi plmp3 rhvoice'
 WIN_PROPERTIES='luwrain player reader windows'
 
 LINUX_ARCH='32 64'
 LINUX_LANGS='en ro ru'
 LINUX_COMPONENTS='base browser interaction-javafx io linux packs pim player reader studio'
-LINUX_APPS='commander contacts mail news notepad twitter viewer vk'
+LINUX_APPS='commander contacts mail news notepad telegram twitter viewer vk'
 LINUX_EXTENSIONS='cmdtts emacspeak plmp3 rhvoice speechd voiceman'
 LINUX_PROPERTIES='linux luwrain player reader'
 LINUX_SCRIPTS='linux'
@@ -31,7 +31,7 @@ LINUX_SCRIPTS='linux'
 ISO_ARCH='64'
 ISO_LANGS='en ro ru'
 ISO_COMPONENTS='base browser interaction-javafx io linux packs pim player reader studio'
-ISO_APPS='commander contacts mail news notepad twitter viewer vk'
+ISO_APPS='commander contacts mail news notepad telegram twitter viewer vk'
 ISO_EXTENSIONS='cmdtts emacspeak plmp3 rhvoice speechd voiceman'
 ISO_PROPERTIES='linux luwrain player reader'
 ISO_SCRIPTS='linux'
