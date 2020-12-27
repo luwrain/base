8f915b377d0ee66720b091e726306a6fb599ce9a
Michael Pozhidaev <msp@luwrain.org>
2020-12-27 14:26:56 +0700

    Adding publisher to the list of components

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,7 +1,7 @@
 
 VERSION=1.9.01
 REGISTRY_VERSION=1.9.01
-GITS='base browser extensions i18n io interaction-javafx linux luwrain packs pim player reader registry studio windows'
+GITS='base browser extensions i18n io interaction-javafx linux luwrain packs pim player publisher reader registry studio windows'
 PROPERTIES_FILE=release.properties
 JUNIT_COMPONENTS='base luwrain linux browser io pim app-notepad'
 SDK_JARS='base browser io pim studio'
@@ -11,18 +11,18 @@ SRC_DEST_DIR=/tmp/luwrain-src
 LANGS='en fr ro ru'
 APPS='commander contacts mail news notepad telegram twitter viewer vk'
 EXTENSIONS='cmdtts emacspeak mssapi plmp3 rhvoice speechd voiceman'
-COMPONENTS='base io browser linux packs pim player reader studio windows interaction-javafx'
+COMPONENTS='base io browser linux packs pim player publisher reader studio windows interaction-javafx'
 
 WIN_ARCH=64
 WIN_LANGS='en ro ru'
-WIN_COMPONENTS='base browser interaction-javafx io packs pim player reader studio windows'
+WIN_COMPONENTS='base browser interaction-javafx io packs pim player publisher reader studio windows'
 WIN_APPS='commander contacts mail news notepad telegram twitter viewer vk'
 WIN_EXTENSIONS='mssapi plmp3 rhvoice'
 WIN_PROPERTIES='luwrain player reader windows'
 
 LINUX_ARCH='64'
 LINUX_LANGS='en ro ru'
-LINUX_COMPONENTS='base browser interaction-javafx io linux packs pim player reader studio'
+LINUX_COMPONENTS='base browser interaction-javafx io linux packs pim player publisher reader studio'
 LINUX_APPS='commander contacts mail news notepad telegram twitter viewer vk'
 LINUX_EXTENSIONS='cmdtts emacspeak plmp3 rhvoice speechd voiceman'
 LINUX_PROPERTIES='linux luwrain player reader'
@@ -30,7 +30,7 @@ LINUX_SCRIPTS='linux'
 
 ISO_ARCH='64'
 ISO_LANGS='en ro ru'
-ISO_COMPONENTS='base browser interaction-javafx io linux packs pim player reader studio'
+ISO_COMPONENTS='base browser interaction-javafx io linux packs pim player publisher reader studio'
 ISO_APPS='commander contacts mail news notepad telegram twitter viewer vk'
 ISO_EXTENSIONS='cmdtts emacspeak plmp3 rhvoice speechd voiceman'
 ISO_PROPERTIES='linux luwrain player reader'
