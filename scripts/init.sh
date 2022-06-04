69f0b241f836307cd30239680826447dcffcfaa9
Michael Pozhidaev <msp@luwrain.org>
2022-06-05 06:47:59 +0700

    Adding lwr-build-mvn

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,6 +3,10 @@
 
 THIS="${0##*/}"
 
+if [ -r base/scripts/config.sh ]; then
+    cd base/scripts
+fi
+
 . config.sh
 
 PWD=$(pwd)
@@ -12,9 +16,9 @@ fi
 SCRIPTS_DIR="$SRC_DIR/base/scripts"
 
 if ! [ -e "$SRC_DIR/base/scripts/init.sh" ]; then
-    echo "$THIS:Something wrong with the current directory:no $SRC_DIR/base/scripts/init.sh"
+    echo "ERROR: $THIS: Something wrong with the current directory:no \$SRC_DIR/base/scripts/init.sh" >&2
     exit 1
 fi
 
-cd "$SRC_DIR"
 export PATH="$SCRIPTS_DIR:$PATH"
+cd "$SRC_DIR"
