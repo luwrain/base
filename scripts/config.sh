43f826b7aaf8e58b57ce9f9a120af2fe80cbf474
Michael Pozhidaev <msp@luwrain.org>
2022-10-05 04:06:19 +0700

    Disabling gpg for Maven

--- scripts/config.sh
+++ scripts/config.sh
@@ -7,7 +7,7 @@ REGISTRY_VERSION=2.0
 LANGS='en ru'
 APPS='commander contacts mail news notepad telegram twitter viewer vk'
 EXTENSIONS='cmdtts emacspeak mssapi plmp3 rhvoice speechd voiceman'
-GITS='base browser extensions i18n io interaction-javafx linux luwrain packs pim player reader registry studio windows'
+GITS='base browser extensions i18n io interaction-javafx linux luwrain pim player reader registry studio windows'
 
 WIN_ARCH=64
 LINUX_ARCH='64'
@@ -18,7 +18,7 @@ SRC_DEST_DIR=/tmp/luwrain-src
 
 ISO_ARCH='64'
 ISO_LANGS='en ro ru'
-ISO_COMPONENTS='base browser interaction-javafx io linux packs pim player reader studio'
+ISO_COMPONENTS='base browser interaction-javafx io linux pim player reader studio'
 ISO_APPS='commander contacts mail news notepad telegram twitter viewer vk'
 ISO_EXTENSIONS='cmdtts emacspeak plmp3 rhvoice speechd voiceman'
 ISO_PROPERTIES='linux luwrain player reader'
