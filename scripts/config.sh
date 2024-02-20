66081cdce8c9732a3914580fba14d06e0e3ccd13
Michael Pozhidaev <msp@luwrain.org>
2024-02-20 09:28:14 +0700

    Removing internal JVM for the linux release

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,17 +1,10 @@
 # Copyright 2012-2024 Michael Pozhidaev <msp@luwrain.org>
 # The LUWRAIN Project, GPL v.3
 
-VERSION=2.0.0
+VERSION=2.0.0-rc2
 REGISTRY_VERSION=2.0
-
 GITS='base browser extensions grammar i18n io interaction-javafx linux luwrain pim player reader registry studio windows'
 APPS='commander contacts mail news notepad telegram viewer vk'
 LANGS='en ru'
 EXTENSIONS='cmdtts emacspeak mssapi plmp3 rhvoice speechd voiceman'
-
-
-WIN_ARCH=64
-LINUX_ARCH='64'
-
-JUNIT_COMPONENTS='base luwrain linux browser io pim app-notepad'
 SRC_DEST_DIR=/tmp/luwrain-src
