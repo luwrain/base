8e0d87c894a91ec4d957043253a9e3db3ba02e61
Michael Pozhidaev <msp@luwrain.org>
2024-02-27 14:34:14 +0700

    Improving the startup sound

--- sounds/startup.sh
+++ sounds/startup.sh
@@ -3,13 +3,19 @@
 # The LUWRAIN Project, GPL v.3
 # Ges major
 
-
-NOTE=30
-DRUM_VOL=-3
+DRUM_VOL=-2
 DRUM_DELAY=0.0
 HARM_DELAY=0.12
-HARM_VOL=-5
-BASS=10
+HARM_VOL=-10
+BASS=5
+
+NOTE=30
+
+INST=11
+DUR=300
+
+NOTE1_VOL=-10
+NOTE2_VOL=-15
 
 cat <<EOF | csvmidi > drum.midi
 0, 0, Header, 1, 2, 480
@@ -35,17 +41,31 @@ cat <<EOF | csvmidi > drum.midi
 0, 0, End_of_file
 EOF
 
-timidity -Ow drum.midi
+timidity -Ow drum.midi > /dev/null
 mv drum.wav _drum1.wav
 sox --norm=$DRUM_VOL _drum1.wav -r 48000 -c 1 _drum2.wav pad $DRUM_DELAY 5 bass $BASS
 sox _drum2.wav -c 2 _drum.wav reverb 85 50 100 100 50 5
 
-sox -D -n -r 48000 -c 2 -b 16 _harm.wav \
+sox -D -n -r 48000 -c 2 -b 16 _harm1.wav \
     synth 10 sin %1 sin %4 sin %9 \
-    fade t 0.4 2.0 1.5 \
-    pad $HARM_DELAY 3 \
-    reverb 100 gain $HARM_VOL
+    fade t 0.4 2 1.5 pad $HARM_DELAY 3 \
+    gain $HARM_VOL
+sox -D _harm1.wav _harm.wav reverb 100 100 100 100 20 5
 
-sox -D _drum.wav _harm.wav -m _startup.wav
+./melody.sh $INST 120 90 $DUR | csvmidi - > _note1-1.midi
+timidity -Ow _note1-1.midi > /dev/null
+sox -D --norm=$NOTE1_VOL _note1-1.wav -r 48000 _note1.wav \
+    gain -10 \
+    reverb 75 \
+    pad 0.3 3
+
+./melody.sh $INST 120 97 $DUR | csvmidi - > _note2-1.midi
+timidity -Ow _note2-1.midi > /dev/null
+sox -D --norm=$NOTE2_VOL _note2-1.wav -r 48000 _note2.wav \
+    reverb 75 \
+    pad 1.2 3
+
+
+sox -D _drum.wav _harm.wav _note1.wav _note2.wav -m _startup.wav
 sox -D --norm=-0.1 _startup.wav -r 44100 startup.wav fade q 0 3 3
 rm -f *.midi _*.wav
