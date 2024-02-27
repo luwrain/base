8159053dd66fd6252cde7544ee8bf54acfac243f
Michael Pozhidaev <msp@luwrain.org>
2024-02-28 05:56:34 +0700

    New improvements of the startup sound

--- sounds/startup.sh
+++ sounds/startup.sh
@@ -14,8 +14,8 @@ NOTE=30
 INST=11
 DUR=300
 
-NOTE1_VOL=-20
-NOTE2_VOL=-25
+NOTE1_VOL=-15
+NOTE2_VOL=-18
 
 cat <<EOF | csvmidi > drum.midi
 0, 0, Header, 1, 4, 480
@@ -73,10 +73,17 @@ sox -D --norm=$NOTE2_VOL _note2-1.wav -r 48000 _note2.wav \
 
 sox -D -n -r 48000 -c 1 -b 16 _noise1.wav \
     synth 1 br synth 0.4 sin fmod %-39-%-15 bass 30 \
-    fade t 0.32 0.52 0.2 \
-    gain -3
+    fade t 0.32 0.52 0.2
 sox -D _noise1.wav -c 2 _noise.wav
 
-sox -D _drum.wav _harm.wav _noise.wav _note1.wav _note2.wav -m _startup.wav
+LONG=110
+./melody.sh 9 120 \
+	    75 $LONG 90 $LONG 101 $LONG 119 $LONG 85 $LONG 98 $LONG 93 $LONG 80 $LONG 101 $LONG 95 $LONG 120 $LONG 104 $LONG 87 $LONG 91 $LONG 89 $LONG | csvmidi - > _high1.midi
+timidity -Ow _high1.midi > /dev/null
+sox -D --norm=-25 _high1.wav -r 48000 _high.wav pad 0.5 \
+    REVERB 100 50 100 100 0 10 \
+fade l 0 10 10
+
+sox -D _drum.wav _harm.wav _noise.wav _high.wav _note1.wav _note2.wav -m _startup.wav
 sox -D --norm=-0.1 _startup.wav -r 44100 startup.wav fade q 0 4.3 4
 rm -f *.midi _*.wav
