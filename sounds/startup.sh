146a2620bdf692ef1d805aafb6829e4ff2e277a0
Michael Pozhidaev <msp@luwrain.org>
2024-02-27 19:07:33 +0700

    New improving of the startup sound

--- sounds/startup.sh
+++ sounds/startup.sh
@@ -7,7 +7,7 @@ DRUM_VOL=-2
 DRUM_DELAY=0.0
 HARM_DELAY=0.12
 HARM_VOL=-10
-BASS=5
+BASS=10
 
 NOTE=30
 
@@ -18,7 +18,7 @@ NOTE1_VOL=-20
 NOTE2_VOL=-25
 
 cat <<EOF | csvmidi > drum.midi
-0, 0, Header, 1, 2, 480
+0, 0, Header, 1, 4, 480
 1, 0, Start_track
 1, 0, Title_t, "Sound icon"
 1, 0, Text_t, "LUWRAIN sound icon"
@@ -34,10 +34,16 @@ cat <<EOF | csvmidi > drum.midi
 2, 300, End_track
 3, 0, Start_track
 3, 0, Instrument_name_t, "MIDI instrument"
-3, 0, Program_c, 1, 115
-3, 0, Note_on_c, 1, $NOTE, 120
+3, 0, Program_c, 1, 117
+3, 0, Note_on_c, 1, $NOTE, 60
 3, 300, Note_off_c, 1, $NOTE, 0
 3, 300, End_track
+4, 0, Start_track
+4, 0, Instrument_name_t, "MIDI instrument"
+4, 0, Program_c, 1, 55
+4, 0, Note_on_c, 1, 66, 40
+4, 300, Note_off_c, 1, 66, 0
+4, 300, End_track
 0, 0, End_of_file
 EOF
 
@@ -66,5 +72,5 @@ sox -D --norm=$NOTE2_VOL _note2-1.wav -r 48000 _note2.wav \
     reverb 100  50 100 100 0 10
 
 sox -D _drum.wav _harm.wav _note1.wav _note2.wav -m _startup.wav
-sox -D --norm=-0.1 _startup.wav -r 44100 startup.wav fade q 0 3 3
+sox -D --norm=-0.1 _startup.wav -r 44100 startup.wav fade q 0 4 4
 rm -f *.midi _*.wav
