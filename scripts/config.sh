75455ab3d7541db1d8d5e605bb54188007ca9490
Michael Pozhidaev <msp@luwrain.org>
2024-04-18 13:37:19 +0700

    Version increment to 2.0.0pre2

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,7 +1,7 @@
 # Copyright 2012-2024 Michael Pozhidaev <msp@luwrain.org>
 # The LUWRAIN Project, GPL v.3
 
-VERSION=2.0.0-rc1
+VERSION=2.0.0pre2
 REGISTRY_VERSION=2.0
 GITS='base browser extensions grammar i18n io interaction-javafx linux luwrain pim player reader registry studio windows'
 APPS='commander contacts mail news notepad telegram viewer vk'
