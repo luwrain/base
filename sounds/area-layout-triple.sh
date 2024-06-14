6449fbb5c0a4948774e2e174a3b1d1120afab6c6
Michael Pozhidaev <msp@luwrain.org>
2024-06-14 10:02:39 +0700

    Sounds fixes

--- sounds/area-layout-triple.sh
+++ sounds/area-layout-triple.sh
@@ -5,13 +5,13 @@
 
 INS=78
 VOLUME=100
-DUR=120
+DUR=150
 
-./melody.sh $INS $VOLUME 77 $DUR 73 $DUR 77 $DUR | csvmidi - > melody.midi && timidity -Ow melody.midi > /dev/null && mv melody.wav .melody-src.wav
+./melody.sh $INS $VOLUME 73 $DUR 65 $DUR 73 $DUR | csvmidi - > melody.midi && timidity -Ow melody.midi > /dev/null && mv melody.wav .melody-src.wav
 sox -D .melody-src.wav  -r 48000 -c 1 -b 16 .melody-pre.wav bass 5
 sox -D --norm=-0.1 .melody-pre.wav -c 2 .melody.wav reverb 50  fade t 0 2.5 2.5
 
-./melody.sh 65 120 89 80 | csvmidi - > melody.midi && timidity -Ow melody.midi > /dev/null && mv melody.wav .high-src.wav
+./melody.sh 65 120 97 80 | csvmidi - > melody.midi && timidity -Ow melody.midi > /dev/null && mv melody.wav .high-src.wav
 sox -D .high-src.wav  -r 48000 -c 1 -b 16 .high-pre.wav
 sox -D --norm=-5  .high-pre.wav -c 2 .high.wav reverb 70  fade t 0 1.5 1.5
 
--- sounds/area-layout.sh
+++ sounds/area-layout.sh
@@ -7,11 +7,11 @@ INS=78
 VOLUME=100
 DUR=50
 
-./melody.sh $INS $VOLUME 77 $DUR | csvmidi - > melody.midi && timidity -Ow melody.midi > /dev/null && mv melody.wav .melody-src.wav
+./melody.sh $INS $VOLUME 73 $DUR | csvmidi - > melody.midi && timidity -Ow melody.midi > /dev/null && mv melody.wav .melody-src.wav
 sox -D .melody-src.wav  -r 48000 -c 1 -b 16 .melody-pre.wav bass 5
 sox -D --norm=-0.1 .melody-pre.wav -c 2 .melody.wav reverb 50  fade t 0 1.5 1.5
 
-./melody.sh 65 120 89 $DUR | csvmidi - > melody.midi && timidity -Ow melody.midi > /dev/null && mv melody.wav .high-src.wav
+./melody.sh  65 120 97 $DUR | csvmidi - > melody.midi && timidity -Ow melody.midi > /dev/null && mv melody.wav .high-src.wav
 sox -D .high-src.wav  -r 48000 -c 1 -b 16 .high-pre.wav
 sox -D --norm=-5  .high-pre.wav -c 2 .high.wav reverb 70  fade t 0 1.5 1.5
 
