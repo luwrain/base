05acc28fcb6a2303ae8026c6d0cda68535718cfe
Michael Pozhidaev <msp@luwrain.org>
2024-06-20 14:28:27 +0700

    Adding lwr-dpkg-snapshot

--- sounds/area-layout.sh
+++ sounds/area-layout.sh
@@ -11,7 +11,7 @@ DUR=50
 sox -D .melody-src.wav  -r 48000 -c 1 -b 16 .melody-pre.wav bass 5
 sox -D --norm=-0.1 .melody-pre.wav -c 2 .melody.wav reverb 50  fade t 0 1.5 1.5
 
-./melody.sh  65 120 97 $DUR | csvmidi - > melody.midi && timidity -Ow melody.midi > /dev/null && mv melody.wav .high-src.wav
+./melody.sh  76 120 73 120 | csvmidi - > melody.midi && timidity -Ow melody.midi > /dev/null && mv melody.wav .high-src.wav
 sox -D .high-src.wav  -r 48000 -c 1 -b 16 .high-pre.wav
 sox -D --norm=-5  .high-pre.wav -c 2 .high.wav reverb 70  fade t 0 1.5 1.5
 
